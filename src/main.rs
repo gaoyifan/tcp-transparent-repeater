29655d8406554da90a3651d5ba8264bfc9169611
Yifan Gao <git@yfgao.com>
2022-03-26 22:09:12 +0800

    Cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ use futures::FutureExt;
 use std::env;
 use std::error::Error;
 
-use nix::sys::socket::{sockopt,getsockopt,InetAddr};
-use std::os::unix::io::AsRawFd;
+use nix::sys::socket::{getsockopt, sockopt, InetAddr};
 use std::net::SocketAddr;
+use std::os::unix::io::AsRawFd;
 
 use bytes::BytesMut;
 
@@ -28,14 +28,23 @@ async fn main() -> Result<(), Box<dyn Error>> {
     while let Ok((inbound, client_addr)) = listener.accept().await {
         let server_addr = getsockopt(inbound.as_raw_fd(), sockopt::OriginalDst).unwrap();
         let server_addr = InetAddr::V4(server_addr).to_std();
-        eprintln!("[INFO]{} -> {}: connection incoming", client_addr, server_addr);
+        eprintln!(
+            "[INFO]{} -> {}: connection incoming",
+            client_addr, server_addr
+        );
         if inbound.local_addr().unwrap() == server_addr {
-            eprintln!("[WARM]{} -> {}: connection from local network", client_addr, server_addr);
+            eprintln!(
+                "[WARM]{} -> {}: connection from local network",
+                client_addr, server_addr
+            );
             continue;
         }
         let transfer = transfer(inbound, server_addr).map(move |r| {
             if let Err(e) = r {
-                println!("[INFO]{} -> {}: connection closed with error; {}", client_addr, server_addr, e);
+                println!(
+                    "[INFO]{} -> {}: connection closed with error; {}",
+                    client_addr, server_addr, e
+                );
             }
         });
         tokio::spawn(transfer);
@@ -56,25 +65,30 @@ async fn transfer(mut inbound: TcpStream, proxy_addr: SocketAddr) -> Result<(), 
     let (ri, wi) = inbound.split();
     let (ro, wo) = outbound.split();
 
-    tokio::try_join!(
-        copy_one_direction(ro, wi),
-        copy_one_direction(ri, wo)
-    )?;
-    eprintln!("[INFO]{} -> {}: connection closed", client_addr, server_addr);
+    tokio::try_join!(copy_one_direction(ro, wi), copy_one_direction(ri, wo))?;
+    eprintln!(
+        "[INFO]{} -> {}: connection closed",
+        client_addr, server_addr
+    );
 
     Ok(())
 }
 
 async fn copy_one_direction<T, U>(mut from: T, mut to: U) -> std::io::Result<()>
-    where T: AsyncReadExt + Unpin, U: AsyncWriteExt + Unpin
+where
+    T: AsyncReadExt + Unpin,
+    U: AsyncWriteExt + Unpin,
 {
     let mut buf = BytesMut::with_capacity(BUF_SIZE);
     loop {
         let n = from.read_buf(&mut buf).await?;
-        if n == 0 { break }
+        if n == 0 {
+            break;
+        }
         let n = to.write_buf(&mut buf).await?;
-        if n == 0 { break }
+        if n == 0 {
+            break;
+        }
     }
     to.shutdown().await
 }
-
